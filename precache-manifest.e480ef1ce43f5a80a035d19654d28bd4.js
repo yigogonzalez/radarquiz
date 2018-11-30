49bdb8edd9ccd7a50546e0ae72cb737a536a82e7
Rodrigo Gonzalez <rgonzale@thoughtworks.com>
2018-11-30 15:26:55 -0300

    Updates

---
+++ precache-manifest.e480ef1ce43f5a80a035d19654d28bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be62237f20a83108a83b",
+    "url": "/radarquiz/static/js/runtime~main.be62237f.js"
+  },
+  {
+    "revision": "c1e6ae7b6d2988fd6db6",
+    "url": "/radarquiz/static/js/main.c1e6ae7b.chunk.js"
+  },
+  {
+    "revision": "0799edb2d35b1519dff5",
+    "url": "/radarquiz/static/js/1.0799edb2.chunk.js"
+  },
+  {
+    "revision": "c1e6ae7b6d2988fd6db6",
+    "url": "/radarquiz/static/css/main.d764d451.chunk.css"
+  },
+  {
+    "revision": "0799edb2d35b1519dff5",
+    "url": "/radarquiz/static/css/1.72845027.chunk.css"
+  },
+  {
+    "revision": "d19385ff564be37c6e7fee22adffc228",
+    "url": "/radarquiz/index.html"
+  }
+];
